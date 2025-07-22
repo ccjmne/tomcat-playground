b9d3c3ffc4fac6e5f441e2c338787f2c7aacfa5d
Éric NICOLAS <ccjmne@gmail.com>
2025-07-22 16:28:59 +0200

    Prettify output

--- src/main/java/org/example/TestServletJavax.java
+++ src/main/java/org/example/TestServletJavax.java
@@ -13,13 +13,15 @@ public class TestServletJavax extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		final PrintWriter out = resp.getWriter();
 		resp.setContentType("text/plain");
-		out.println(String.format("%20s: %s", "getRequestURL",  req.getRequestURL()));
-		out.println(String.format("%20s: %s", "getQueryString", req.getQueryString()));
-		out.println(String.format("%20s: %s", "getServerName",  req.getServerName()));
-		out.println(String.format("%20s: %s", "getServerPort",  req.getServerPort()));
-		out.println(String.format("%20s: %s", "getScheme",      req.getScheme()));
-		out.println(String.format("%20s: %s", "isSecure",       req.isSecure()));
-		out.println(String.format("%20s: %s", "getRemoteAddr",  req.getRemoteAddr()));
+		out.println(String.format("%20s  %s", "", "------------ METHODS ------------"));
+		out.println(String.format("%20s: %s", "getRequestURL()",  req.getRequestURL()));
+		out.println(String.format("%20s: %s", "getRemoteAddr()",  req.getRemoteAddr()));
+		out.println(String.format("%20s: %s", "getServerName()",  req.getServerName()));
+		out.println(String.format("%20s: %s", "getServerPort()",  req.getServerPort()));
+		out.println(String.format("%20s: %s", "getScheme()",      req.getScheme()));
+		out.println(String.format("%20s: %s", "isSecure()",       req.isSecure()));
+		out.println(String.format("%20s: %s", "getQueryString()", req.getQueryString()));
+		out.println(String.format("%20s  %s", "", "------------ HEADERS ------------"));
 		req.getHeaderNames().asIterator().forEachRemaining(k -> req.getHeaders(k).asIterator()
 				.forEachRemaining(v -> out.println(String.format("%20s: %s", k, v))));
 	}
