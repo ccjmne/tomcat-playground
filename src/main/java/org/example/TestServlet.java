b8f4d0cdd4dcd5bc0ee840d981043a54384b38a3
Éric NICOLAS <ccjmne@gmail.com>
2025-07-22 16:06:19 +0200

    Polish servlet code, realign both versions

--- src/main/java/org/example/TestServlet.java
+++ src/main/java/org/example/TestServlet.java
@@ -11,15 +11,15 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/test")
 public class TestServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		final PrintWriter out = resp.getWriter();
 		resp.setContentType("text/plain");
-        final PrintWriter out = resp.getWriter();
-		out.println(String.format("%20s: %s", "getRequestURL", req.getRequestURL()));
+		out.println(String.format("%20s: %s", "getRequestURL",  req.getRequestURL()));
 		out.println(String.format("%20s: %s", "getQueryString", req.getQueryString()));
-		out.println(String.format("%20s: %s", "getServerName", req.getServerName()));
-		out.println(String.format("%20s: %s", "getServerPort", req.getServerPort()));
-		out.println(String.format("%20s: %s", "getScheme", req.getScheme()));
-		out.println(String.format("%20s: %s", "isSecure", req.isSecure()));
-		out.println(String.format("%20s: %s", "getRemoteAddr", req.getRemoteAddr()));
+		out.println(String.format("%20s: %s", "getServerName",  req.getServerName()));
+		out.println(String.format("%20s: %s", "getServerPort",  req.getServerPort()));
+		out.println(String.format("%20s: %s", "getScheme",      req.getScheme()));
+		out.println(String.format("%20s: %s", "isSecure",       req.isSecure()));
+		out.println(String.format("%20s: %s", "getRemoteAddr",  req.getRemoteAddr()));
 		req.getHeaderNames().asIterator().forEachRemaining(k -> req.getHeaders(k).asIterator()
 				.forEachRemaining(v -> out.println(String.format("%20s: %s", k, v))));
 	}
--- src/main/java/org/example/TestServletJavax.java
+++ src/main/java/org/example/TestServletJavax.java
@@ -11,9 +11,15 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/test-javax")
 public class TestServletJavax extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
 		final PrintWriter out = resp.getWriter();
-		out.println(String.format("%20s: %s", "getRequestURL", req.getRequestURL()));
+		resp.setContentType("text/plain");
+		out.println(String.format("%20s: %s", "getRequestURL",  req.getRequestURL()));
+		out.println(String.format("%20s: %s", "getQueryString", req.getQueryString()));
+		out.println(String.format("%20s: %s", "getServerName",  req.getServerName()));
+		out.println(String.format("%20s: %s", "getServerPort",  req.getServerPort()));
+		out.println(String.format("%20s: %s", "getScheme",      req.getScheme()));
+		out.println(String.format("%20s: %s", "isSecure",       req.isSecure()));
+		out.println(String.format("%20s: %s", "getRemoteAddr",  req.getRemoteAddr()));
 		req.getHeaderNames().asIterator().forEachRemaining(k -> req.getHeaders(k).asIterator()
 				.forEachRemaining(v -> out.println(String.format("%20s: %s", k, v))));
 	}
