7ee762a05ec4f22182be7afd7b3d61ecffe59086
Éric NICOLAS <ccjmne@gmail.com>
2025-07-22 15:40:30 +0200

    Rename image and container to tomcat-playground

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 # (cd app && mvn clean package)
-docker build -t tomcat-remoteipvalve .
-docker run --rm -it --name tomcat-remoteipvalve -p 8080:8080 tomcat-remoteipvalve
+docker build -t tomcat-playground .
+docker run --rm -it --name tomcat-playground -p 8080:8080 tomcat-playground
