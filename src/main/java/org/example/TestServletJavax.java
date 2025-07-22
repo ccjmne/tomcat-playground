a41673fea027ef32b1dec23c6b42e21c17aad77f
Éric NICOLAS <ccjmne@gmail.com>
2025-07-22 16:06:46 +0200

    Always bind servlet to /test

--- src/main/java/org/example/TestServletJavax.java
+++ src/main/java/org/example/TestServletJavax.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/test-javax")
+@WebServlet("/test")
 public class TestServletJavax extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		final PrintWriter out = resp.getWriter();
