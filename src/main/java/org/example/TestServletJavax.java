6902fa0ad2c428ce58c50c251564ea58923fd91d
Éric NICOLAS <ccjmne@gmail.com>
2025-07-24 15:32:31 +0200

    Relabel and reorder HEADERS and METHODS headings
    Have INCOMING HEADERS (previously HEADERS) come *first*, then have
    INTERPRETED PROPERTIES (previously METHODS) follow logically.

--- src/main/java/org/example/TestServletJavax.java
+++ src/main/java/org/example/TestServletJavax.java
@@ -13,7 +13,10 @@ public class TestServletJavax extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		final PrintWriter out = res.getWriter();
 		res.setContentType("text/plain");
-		out.println(String.format("%20s %s", "", "------------ METHODS ------------"));
+		out.println(String.format("%20s %s", "", "----------- INCOMING HEADERS -----------"));
+		req.getHeaderNames().asIterator().forEachRemaining(k -> req.getHeaders(k).asIterator()
+				.forEachRemaining(v -> out.println(String.format("%19s: %s", k, v))));
+		out.println(String.format("%20s %s", "", "-------- INTERPRETED PROPERTIES --------"));
 		out.println(String.format("%20s %s", "getRequestURL()",  req.getRequestURL()));
 		out.println(String.format("%20s %s", "getRemoteAddr()",  req.getRemoteAddr()));
 		out.println(String.format("%20s %s", "getServerName()",  req.getServerName()));
@@ -21,8 +24,5 @@ public class TestServletJavax extends HttpServlet {
 		out.println(String.format("%20s %s", "getScheme()",      req.getScheme()));
 		out.println(String.format("%20s %s", "isSecure()",       req.isSecure()));
 		out.println(String.format("%20s %s", "getQueryString()", req.getQueryString()));
-		out.println(String.format("%20s %s", "", "------------ HEADERS ------------"));
-		req.getHeaderNames().asIterator().forEachRemaining(k -> req.getHeaders(k).asIterator()
-				.forEachRemaining(v -> out.println(String.format("%19s: %s", k, v))));
 	}
 }
