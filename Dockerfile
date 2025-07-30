0ebbdaa6ca50ed7d8c3423f903f7411fbe37e9ac
Éric NICOLAS <ccjmne@gmail.com>
2025-07-30 18:32:11 +0200

    Enable debug with ./start.sh debug

--- Dockerfile
+++ Dockerfile
@@ -9,3 +9,7 @@ FROM tomcat:11-jre17
 RUN rm -rf webapps/*
 COPY server.xml conf/
 COPY --from=build /app/target/app-1.0-SNAPSHOT.war webapps/ROOT.war
+ARG DEBUG=no
+RUN case $DEBUG in \
+  [yY]*) echo 'CATALINA_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005"' >> bin/setenv.sh ;; \
+esac
--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #! /bin/sh
 
-# (cd app && mvn clean package)
-docker build -t tomcat-playground .
-docker run --rm -it --name tomcat-playground -p 8080:8080 tomcat-playground
+if [ $1 == "debug" ]; then
+    docker build --build-arg DEBUG=yes -t tomcat-playground .
+    docker run --rm -it --name tomcat-playground -p8080:8080 -p5005:5005 tomcat-playground
+else
+    docker build                       -t tomcat-playground .
+    docker run --rm -it --name tomcat-playground -p8080:8080             tomcat-playground
+fi
